d8b1d84b9b39f2753c1b199d643058e5a321158b
masterkais <kais2032@gmail.com>
2021-12-27 05:07:52 +0100

    version beta 1.0.0 for the project spring admin

---
+++ src/main/java/com/iit/imprimerie/SpringAdminApplication.java
@@ -0,0 +1,18 @@
+package com.iit.imprimerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+
+}
